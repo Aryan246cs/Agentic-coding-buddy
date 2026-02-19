99cf885085305f6f4f663a7cc0827e35b1215a7a
Aryan246cs <aryansrivastava24062005@gmail.com>
2026-02-19 23:25:39 +0530

    added Planner and Architect part of graph

--- agent/graph.py
+++ agent/graph.py
@@ -1,15 +1,42 @@
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
-from pydantic import BaseModel,Field
+from pydantic import BaseModel, Field
 
 from agent.prompts import *
 from agent.states import *
+from langgraph.constants import END
+from langgraph.graph import StateGraph
 
 load_dotenv()
-user_prompt = "create a simple calculator web application"
-
-prompt = planner_prompt(user_prompt)
 
 llm = ChatGroq(model="openai/gpt-oss-120b")
-resp = llm.with_structured_output(Plan).invoke(prompt)
-print(resp)
+
+# planner plans everything for the prompt, like tech stack, description, features and files that need to be made
+
+def planner_agent(state: dict) -> dict:
+    user_prompt = state["user_prompt"]
+    resp = llm.with_structured_output(Plan).invoke(planner_prompt(user_prompt))
+    if resp is None:
+        raise ValueError("Planner did'nt return a valid response.")
+    return {"plan": resp}
+
+def architect_agent(state: dict) -> dict:
+    plan: Plan = state["plan"]
+    resp = llm.with_structured_output(TaskPlan).invoke(architect_prompt(plan))
+    if resp is None:
+        raise ValueError("Architect did'nt return a valid response.")
+    resp.plan = plan
+    return {"task_plan": resp}
+
+graph = StateGraph(dict)
+graph.add_node("planner", planner_agent)
+graph.add_node("architect", architect_agent)
+graph.add_edge("planner","architect")
+graph.set_entry_point("planner")
+
+agent = graph.compile()
+
+user_prompt = "create a simple calculator web application"
+
+result = agent.invoke({"user_prompt": user_prompt})
+print(result)
